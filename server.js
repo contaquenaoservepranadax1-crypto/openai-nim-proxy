5e9e1f458b638e0066380bb3693b3d1f638b2178
contaquenaoservepranadax1-crypto <contaquenaoservepranadax1@gmail.com>
2025-10-21 09:27:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const NIM_API_KEY = process.env.NIM_API_KEY;
 const SHOW_REASONING = false;
 
 // THINKING MODE TOGGLE
-const ENABLE_THINKING_MODE = true;
+const ENABLE_THINKING_MODE = false;
 
 // Model mapping
 const MODEL_MAPPING = {
