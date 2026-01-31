c8383d960d60aca035d4c7af32a61522c66ba776
contaquenaoservepranadax1-crypto <contaquenaoservepranadax1@gmail.com>
2026-01-31 17:12:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const ENABLE_THINKING_MODE = false; // Desativado porque prefiro assim
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
-  'gpt-4-turbo': 'deepseek-ai/deepseek-v3.1',
+  'gpt-4-turbo': 'deepseek-ai/deepseek-v3.2',
   'gpt-4o': 'deepseek-ai/deepseek-v3.1-terminus',
   'claude-3-opus': 'openai/gpt-oss-120b',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
