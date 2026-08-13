0d65b08a07dc817d0389cb4421b27cbfa3fdf6e2
contaquenaoservepranadax1-crypto <contaquenaoservepranadax1@gmail.com>
2026-08-13 05:51:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 const NIM_API_BASE =
   process.env.NIM_API_BASE || 'https://integrate.api.nvidia.com/v1';
 
-const NIM_API_KEY = process.env.NVIDIA_SECOND_API_KEY;
+const NIM_API_KEY = process.env.NVIDIA_THIRD_API_KEY;
 const CLIENT_AUTH_KEY = process.env.CLIENT_AUTH_KEY;
 
 const SHOW_REASONING = process.env.SHOW_REASONING === 'true';
